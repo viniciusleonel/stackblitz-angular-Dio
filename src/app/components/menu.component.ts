f014098361e57279315c28e3d3db9a977fa99b6c
viniciusleonel <124933333+viniciusleonel@users.noreply.github.com>
2023-09-11 13:45:25 -0300

    1 file modified

--- src/app/components/menu.component.ts
+++ src/app/components/menu.component.ts
@@ -2,23 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'menu-component',
-  // standalone: true,
   template: '<h2>Vinícius</h2>',
   styles: ['h2 { color: blue }']
 })
 
 export class MenuComponent{}
-
-// import 'zone.js/dist/zone';
-// import { Component } from '@angular/core';
-// import { CommonModule } from '@angular/common';
-// import { bootstrapApplication } from '@angular/platform-browser';
-
-// @Component({
-//   selector: 'menu-component',
-//   standalone: true,
-//   imports: [CommonModule],
-//   template: '<h2>Vinicius</h2>',
-// })
-// export class MenuComponent {}
-
